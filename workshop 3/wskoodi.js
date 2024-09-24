05ad12b722faf026504106dd8d8069e256eaa2d7
Teemu <teemu.markuss@gmail.com>
2024-09-24 13:49:27 +0300

    ex1

---
+++ workshop 3/wskoodi.js
@@ -0,0 +1,57 @@
+//Tehtävä 1
+// Funktio nappi1
+function handleNappi1Click() {
+    const currentDate = new Date();
+    alert('You clicked me!\nCurrent date: '+ currentDate);
+    
+}
+
+// Funktio nappi2
+function handleNappi2Click() {
+    const table=
+    document.getElementById("example"); // Hakee taulukon elementin
+    if (table.style.display === "none") {
+        table.style.display = "table"; // Näyttää taulukon
+    } else {
+        table.style.display = "none"; // Piilottaa taulukon, jos se on näkyvissä
+    }
+}
+
+// Funktio nappi3
+function handleNappi3Click() {
+   if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition, showError);
+   } else {
+    alert('Selain ei tue geolokaatiota')
+   }
+}
+// Function to handle successful retrieval of location
+function showPosition(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    alert(`Your location:\nLatitude: ${latitude}\nLongitude: ${longitude}`);
+}
+
+// Function to handle errors in retrieving location
+function showError(error) {
+    switch (error.code) {
+        case error.PERMISSION_DENIED:
+            alert('User denied the request for Geolocation.');
+            break;
+        case error.POSITION_UNAVAILABLE:
+            alert('Location information is unavailable.');
+            break;
+        case error.TIMEOUT:
+            alert('The request to get user location timed out.');
+            break;
+        case error.UNKNOWN_ERROR:
+            alert('An unknown error occurred.');
+            break;
+    }
+}
+
+
+// Add event listeners to each button
+document.getElementById('nappi1').onclick = handleNappi1Click;
+document.getElementById('nappi2').onclick = handleNappi2Click;
+document.getElementById('nappi3').onclick = handleNappi3Click;
